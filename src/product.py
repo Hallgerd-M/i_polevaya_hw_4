bfa48514e165a08aac3db7c4e1c9649886596804
Irina Polevaya <mirwen@yandex.ru>
2024-07-23 03:34:18 +0300

    task 2 - needhelp

--- src/product.py
+++ src/product.py
@@ -9,5 +9,23 @@ class Product:
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, new_product: dict):
+        """Взвращает созданный объект класса Product из параметров товара в словаре"""
+        name = new_product["name"]
+        description = new_product["description"]
+        price = new_product["price"]
+        quantity = new_product["quantity"]
+        return cls(name, description, price, quantity)
+
+    @property
+    def get_price(self):
+        return self.__price
+
+    @get_price.setter
+    def get_price(self):
+        if self.__price <= 0:
+            print("Цена не должна быть нулевая или орицательная")
